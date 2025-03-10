aceca8b366b912ec75b1687d4ff4db9f7e5894ca
Satyanarayana Chila <satyachila18010@gmail.com>
2025-03-10 21:37:48 +0530

    Improved logic and added comments for better readability

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import os # interacts with system files
 
 path=input("Enter the  Folder Path:") #Taking path as an input
+
+print("processing....")#The get information that the process is started
+
 if os.path.isdir(path): #Checks the folder is present or not
     files = os.listdir(path) # returns the list of files
+    
     if files:
-        print("files exists in the folder")
-        for file in files:
+        print("Files exist in the folder")
+
+        no_of_log_files=0
+        no_of_non_log_files=0
+
+        for file in files:# checks each file in the directory
             if file.endswith(".log"): # checks if the file ends with .log
-                print("log files found")                
+                no_of_log_files += 1
             else:
-                print("It has different extensions")     
+                no_of_non_log_files+=1
+        if no_of_log_files>0 and no_of_non_log_files == 0:# checks for only log files
+            print(f"Log files found:{no_of_log_files}")
+        else:
+            print("It has non log files in it please provide files which has non log files.")   
+            
     else:
-        print("No files in the folder")
-    print("processing")
+        print("Please provide the path which has files in it.")
 else:
-    print("Inavalid Path")
+    print("Inavalid Path, Please provide the correct path.")
 
