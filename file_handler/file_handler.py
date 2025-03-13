12bf02e3e20db4ef4de76071e71d5e8eddd64527
Satyanarayana Chila <satyanarayana.chila@theelixrlabs.com>
2025-03-13 18:28:56 +0530

    refractoring the code

--- file_handler/file_handler.py
+++ file_handler/file_handler.py
@@ -1,11 +1,7 @@
 import os
- 
-def get_files_in_folder(file_path):
-    """
-    Checks if the given folder path exists and returns a list of files in the folder.
 
-    """
+#Checks if the given folder path exists and returns a list of files in the folder.
+def get_files_in_folder(file_path):
     if not os.path.isdir(file_path):
         return None  # Return None if the folder does not exist
- 
     return os.listdir(file_path)  # Return list of files in the folder
--- main.py
+++ main.py
@@ -5,27 +5,21 @@ import sys
 project_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(project_root)
  
-# Import necessary modules
 from config.constants import *
 from file_handler.file_handler import get_files_in_folder
 from log_checker.log_checker import count_log_files
  
 # Take folder path as input
 folder_path = input("Enter the Folder Path: ").strip()
- 
 # Check if the folder exists
 if os.path.isdir(folder_path):
     files = get_files_in_folder(folder_path)  # Get list of files
- 
     if files:
         print(PROCESSING_MESSAGE)
- 
         # Count log and non-log files
         num_log_files, num_non_log_files = count_log_files(files)
- 
         print(f"Log files found: {num_log_files}")
         print(f"Invalid files found: {num_non_log_files}")
- 
         if num_log_files == 0:
             print(NO_LOG_FILES_MESSAGE)
     else:
